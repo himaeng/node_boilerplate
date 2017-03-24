561417cc8c8ec923140d76a036901611f39809a6
Tanakorn Numrubporn <him_aeng@yahoo.co.th>
2017-03-24 14:57:54 +0700

    preparing code

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-import initializeDb from './db';
-import middleware from './middleware';
 import api from './api';
 import config from './config.json';
 
@@ -23,18 +21,6 @@ app.use(bodyParser.json({
 	limit : config.bodyLimit
 }));
 
-// connect to db
-initializeDb( db => {
-
-	// internal middleware
-	app.use(middleware({ config, db }));
-
-	// api router
-	app.use('/api', api({ config, db }));
-
-	app.server.listen(process.env.PORT || config.port);
-
-	console.log(`Started on port ${app.server.address().port}`);
-});
+app.use('/api', api());
 
 export default app;
