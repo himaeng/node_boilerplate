4e71da614cb32836b82e40762e87f26c88fb29c9
Tanakorn Numrubporn <him_aeng@yahoo.co.th>
2017-04-03 08:41:25 +0700

    complete node test tutorial"

--- __tests__/Product-test.js
+++ __tests__/Product-test.js
@@ -15,6 +15,55 @@ describe('FlowAPI', () => {
           expect(response.body).toBeInstanceOf(Array);
         })
     });
+
+    it('should return objs with correct props', () => {
+      return request(app).get('/api/v1/product')
+        .expect(200)
+        .then(response => {
+          let sampleKeys = Object.keys(response.body[0]);
+          expectedProps.forEach((key) => {
+            expect(sampleKeys.includes(key)).toBe(true);
+          })
+        })
+    });
+
+    it('should not return objects with extra props', () => {
+      return request(app).get('/api/v1/product')
+        .expect(200)
+        .then(response => {
+          let extraProps = Object.keys(response.body[0]).filter((key) => {
+            return !expectedProps.includes(key);
+          });
+          expect(extraProps.length).toBe(0);
+        })
+    })
   });
+
+  describe('GET /api/v1/product/:id - get product item by id', () => {
+    it('should return an object of type Product', () => {
+      return request(app).get('/api/v1/product/1')
+        .expect(200)
+        .then((response) => {
+          const reqKeys = ['id', 'name', 'price', 'quantity'];
+          const {item} = response.body;
+          reqKeys.forEach((key) => {
+            expect(Object.keys(item)).toContain(key);
+          });
+        })
+    });
+
+    it('should return a Product with request id', () => {
+      return request(app).get('/api/v1/product/1')
+        .expect(200)
+        .then((response) => {
+          expect(response.body.item).toEqual({
+            "id": 1,
+            "name": "banana",
+            "quantity": 15,
+            "price": 1
+          })
+        })
+    })
+  })
 
 });
--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
-import { version } from '../../package.json';
 import { Router } from 'express';
+
+import { version } from '../../package.json';
+import inventory from '../../data/produce.json';
 
 export default () => {
 	let api = Router();
@@ -11,7 +13,23 @@ export default () => {
 
 	api.get('/v1/product', (req, res) => {
 		res.json(inventory);
-	})
+	});
+
+	api.get('/v1/product/:id', (req, res) => {
+		const id = parseInt(req.params.id, 10);
+		const record = inventory.find(item => item.id === id);
+		if (record) {
+			res.json({
+				message: 'Success',
+				item: record
+			})
+		} else {
+			res.status(400).json({
+				status: res.status,
+				message: `หา item รหัส ${id} ไม่เจอ`
+			})
+		}
+	});
 
 	return api;
 }
